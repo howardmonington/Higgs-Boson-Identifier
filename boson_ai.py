fc34a4ab9a9b361dbcf23a5571d5dd8aecf24b22
Luke Monington <lukemonington3@gmail.com>
2020-08-25 06:05:46 -0600

    prepare data for ai and pass it through xgboost

--- boson_ai.py
+++ boson_ai.py
@@ -1,10 +1,53 @@
 import pandas as pd
 import numpy as np
 pd.set_option('max_columns',None)
+import tensorflow as tf
+import seaborn as sns
+from sklearn.impute import SimpleImputer
+import xgboost as xgb
+from xgboost import XGBClassifier
 
 train = pd.read_csv(r'C:\Users\lukem\Desktop\Github AI Projects\Data for ai competitions\higgs boson ml challenge\training.csv')
 test = pd.read_csv(r'C:\Users\lukem\Desktop\Github AI Projects\Data for ai competitions\higgs boson ml challenge\test.csv')
 sample_submission = pd.read_csv(r'C:\Users\lukem\Desktop\Github AI Projects\Data for ai competitions\higgs boson ml challenge\random_submission.csv')
 
+y = train.Label
+train = train.drop('Label', axis = 1)
+train = train.drop('Weight', axis = 1)
 
-train.describe
+train_cols = train.columns
+test_cols = test.columns
+
+X = train
+del train
+
+X = X.replace(-999.000,np.nan)
+test = test.replace(-999.000,np.nan)
+
+imp = SimpleImputer(missing_values = np.nan, strategy = "mean")
+imp.fit(X)
+X = pd.DataFrame(imp.transform(X))
+
+imp.fit(test)
+test = pd.DataFrame(imp.transform(test))
+
+X.columns = train_cols
+test.columns = test_cols
+
+sns.set(style="darkgrid")
+ax = sns.barplot(x = y.value_counts().index, y = y.value_counts())
+
+clf = xgb.XGBClassifier(
+    n_estimators=500,
+    max_depth=9,
+    learning_rate=0.05,
+    subsample=0.9,
+    colsample_bytree=0.9,
+    missing=-999,
+    random_state=2019,
+    tree_method='gpu_hist'  # THE MAGICAL PARAMETER
+)
+
+clf.fit(X, y)
+
+pred = clf.predict(test)
